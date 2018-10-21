e111869dddfd6078bc1e59fd850cbd4390df7d14
Yoshihide Jimbo <yjimbo@gmail.com>
2018-10-21 21:58:43 +0900

    feat: first release

--- test/module.test.js
+++ test/module.test.js
@@ -1,26 +1,92 @@
-const { Nuxt, Builder } = require("nuxt");
-const request = require("request-promise-native");
+const { Nuxt, Builder, Generator } = require('nuxt');
+const fs = require('fs');
 
-const config = require("./fixture/nuxt.config");
+const distDir = `${__dirname}/../dist`;
+const headersFilePath = `${distDir}/_headers`;
 
-const url = path => `http://localhost:3000${path}`;
-const get = path => request(url(path));
-
-describe("basic", () => {
+describe('no `resources` property', () => {
   let nuxt;
+  const config = require('./fixture/nuxt.config.no-resource');
 
   beforeAll(async () => {
     nuxt = new Nuxt(config);
-    await new Builder(nuxt).build();
-    await nuxt.listen(3000);
+    const builder = new Builder(nuxt);
+    await new Generator(nuxt, builder).generate();
   }, 60000);
 
-  afterAll(async () => {
-    await nuxt.close();
+  test('skip generating _headers file', async () => {
+    const headersFilePath = `${distDir}/_headers`;
+    expect(fs.existsSync(headersFilePath)).toBeFalsy();
+  });
+});
+
+describe('basic', () => {
+  let nuxt;
+  const config = require('./fixture/nuxt.config');
+
+  beforeAll(async () => {
+    nuxt = new Nuxt(config);
+    const builder = new Builder(nuxt);
+    await new Generator(nuxt, builder).generate();
+  }, 60000);
+
+  test('generate a new _headers file', async () => {
+    const files = [];
+    expect(fs.existsSync(headersFilePath)).toBeTruthy();
+    const data = await fs.readFileSync(headersFilePath, 'utf8');
+    fs.readdirSync(`${distDir}/_nuxt/`, 'utf8').map(file => {
+      if (file.match(/\.js$/)) {
+        files.push(file);
+      }
+    });
+    fs.readdirSync(`${distDir}/images/`, 'utf8').map(file => {
+      if (file.match(/\.jpg$/)) {
+        files.push(file);
+      }
+    });
+    files.forEach(file => {
+      expect(data).toMatch(new RegExp(file));
+    });
+  });
+});
+
+describe('append', () => {
+  let nuxt;
+  const config = require('./fixture/nuxt.config');
+
+  beforeAll(async () => {
+    fs.copyFileSync(
+      `${__dirname}/fixture/_headers`,
+      `${__dirname}/fixture/static/_headers`
+    );
+
+    nuxt = new Nuxt(config);
+    const builder = new Builder(nuxt);
+    await new Generator(nuxt, builder).generate();
+  }, 60000);
+
+  afterAll(() => {
+    fs.unlinkSync(`${__dirname}/fixture/static/_headers`);
   });
 
-  test("render", async () => {
-    let html = await get("/");
-    expect(html).toContain("Works!");
+  test('append to the _headers file', async () => {
+    const files = [];
+    expect(fs.existsSync(headersFilePath)).toBeTruthy();
+    const data = await fs.readFileSync(headersFilePath, 'utf8');
+    fs.readdirSync(`${distDir}/_nuxt/`, 'utf8').map(file => {
+      if (file.match(/\.js$/)) {
+        files.push(file);
+      }
+    });
+    fs.readdirSync(`${distDir}/images/`, 'utf8').map(file => {
+      if (file.match(/\.jpg$/)) {
+        files.push(file);
+      }
+    });
+    files.forEach(file => {
+      expect(data).toMatch(new RegExp(file));
+    });
+    expect(data).toMatch('X-Frame-Options: DENY');
+    expect(data).toMatch('X-XSS-Protection: 1; mode=block');
   });
 });
