be084b51c7f6721e12ee05f82db4ec40e395b7ff
Yoshihide Jimbo <yjimbo@gmail.com>
2018-10-21 15:55:32 +0900

    chore: integrate prettier into eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
   root: true,
   parserOptions: {
-    sourceType: 'module'
+    sourceType: "module"
   },
   env: {
     browser: true,
     node: true,
     jest: true
   },
-  extends: 'standard',
-  plugins: [
-    'jest',
-    'vue'
-  ],
+  extends: ["standard", "plugin:prettier/recommended"],
+  plugins: ["jest", "vue", "prettier"],
   rules: {
-    // Allow paren-less arrow functions
-    'arrow-parens': 0,
-    // Allow async-await
-    'generator-star-spacing': 0,
-    // Allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // Do not allow console.logs etc...
-    'no-console': 2
+    // // Allow paren-less arrow functions
+    // 'arrow-parens': 0,
+    // // Allow async-await
+    // 'generator-star-spacing': 0,
+    // // Allow debugger during development
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // // Do not allow console.logs etc...
+    // 'no-console': 2,
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false
+      }
+    ]
   },
   globals: {
-    'jest/globals': true,
+    "jest/globals": true,
     jasmine: true
   }
 }
